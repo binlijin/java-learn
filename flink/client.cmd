6c4bbbbfa338193b3f38e6f7d97b4c274c64fd9c
binlijin <binlijin@gmail.com>
2018-05-17 19:35:51 +0800

    Create client.cmd

---
+++ flink/client.cmd
@@ -0,0 +1,8 @@
+Apache Flink流作业提交流程分析  
+http://vinoyang.com/2016/07/17/flink-streaming-job-submit/
+
+Flink运行时之客户端提交作业图-上  
+http://vinoyang.com/2017/03/31/flink-runtime-client-submit-jobgraph/
+
+Flink运行时之客户端提交作业图-下  
+http://vinoyang.com/2017/04/02/flink-runtime-client-submit-jobgraph-submitJob-method/
