a85f757e9752365f5dc35335758aa0ae726a025d
binlijin <binlijin@gmail.com>
2018-06-14 14:57:22 +0800

    Update client.cmd

--- flink/client.cmd
+++ flink/client.cmd
@@ -7,5 +7,8 @@ http://vinoyang.com/2017/03/31/flink-runtime-client-submit-jobgraph/
 Flink运行时之客户端提交作业图-下  
 http://vinoyang.com/2017/04/02/flink-runtime-client-submit-jobgraph-submitJob-method/
 
+Flink on Yarn源码(v1.4-SNAPSHOT)分析
+http://yoelee.com/2017/12/18/Flink_on_Yarn%E6%BA%90%E7%A0%81%E5%88%86%E6%9E%90/
+
 CliFrontend
 FlinkYarnSessionCli
